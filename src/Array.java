728ee6aa1d6f3933db222a521889818b4ed91866
cohesivepuma233 <546053862@qq.com>
2024-11-27 23:29:47 +0800

    11.27.00

---
+++ src/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args){
+        int[] nums;
+        nums = new int[7];
+        nums[0] = 11;
+        nums[1] = 12;
+        nums[2] = 14;
+        nums[3] = 22;
+        nums[4] = 45;
+        nums[5] = 36;
+        nums[6] = 39;
+        for(int i=0;i<7;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
---
+++ src/lanqiao.java
@@ -0,0 +1,6 @@
+public class lanqiao {
+    public static void main(String[] args){
+        byte a = 3;
+        System.out.println(a);
+    }
+}
